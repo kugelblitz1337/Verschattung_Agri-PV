70561da6c18e9d28d3fb60de265d19c7684c0fc5
pr0ga <130856563+pr0ga@users.noreply.github.com>
2025-06-25 14:54:42 +0200

    Update AgriPVData.java

--- src/agri/pv_shadow_sim_java/AgriPVData.java
+++ src/agri/pv_shadow_sim_java/AgriPVData.java
@@ -1,6 +1,7 @@
 package agri.pv_shadow_sim_java;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import org.locationtech.jts.geom.*;
 
 /**
@@ -11,8 +12,7 @@ public class AgriPVData implements Serializable{
     
     protected String gemeindename;
     protected int flurstueckszaehler;
-    protected int flurstuecksnenner;
-    
+    protected int flurstuecksnenner;    
     protected double[][] grundstuecksgrenze;
     protected int points;
     protected double mine;
@@ -20,7 +20,14 @@ public class AgriPVData implements Serializable{
     protected double maxe;
     protected double maxn;
     
-    protected int[][] mitlpuktPV;
+    protected ArrayList<Coordinate> mitlpuktPV;
     protected Polygon plotPolygon;
     
+    protected double[][] shadingValues; // Speichert die kumulierten Verschattungswerte (z.B. Schattenminuten)
+    protected Polygon[][] gridPolygons; // Speichert die JTS-Polygone für jedes Gitternetzfeld
+    
+    public static double dmsToDecimal(int grad, int minuten, double sekunden) {
+        double dezimal = grad + minuten / 60.0 + sekunden / 3600.0;
+        return dezimal;
+    }
 }
